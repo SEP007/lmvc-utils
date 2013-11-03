ef6769d58d2b1dd5f3c87049b0b8e4e1ec3998f5
Tobias Deekens <nerd@tdeekens.name>
2013-11-03 17:52:17 +0100

    Adds implementation for filescribe.

--- lib/Scandio/lmvc/utils/logger/scribes/FileScribe.php
+++ lib/Scandio/lmvc/utils/logger/scribes/FileScribe.php
@@ -5,7 +5,8 @@ namespace Scandio\lmvc\utils\logger\scribes;
 class FileScribe extends AbstractScribe
 {
     private
-        $_logPath = null;
+        $_logPath       = null,
+        $_fileObject    = null;
 
     public function scribe($message, $context, $level)
     {
@@ -18,6 +19,16 @@ class FileScribe extends AbstractScribe
 
     private function _write($message)
     {
+        $this->_fileObject->fwrite($message);
+    }
+
+    private function _openStream()
+    {
+        try {
+            $this->_fileObject = new \SplFileObject($this->_logPath, 'a');
+        } catch (\RuntimeException $ex) {
+            trigger_error('Logger scribe ' . __CLASS__ . ' could not open stream: ' . $this->_logPath . '.', E_WARNING);
+        }
 
     }
 
@@ -25,5 +36,7 @@ class FileScribe extends AbstractScribe
     {
         $this->_logPath = $config->path;
         $this->setLevel($config->level);
+
+        $this->_openStream();
     }
 }
