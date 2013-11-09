af44394ac2585ac3ae1b9ecf14338550b960ff59
Tobias Deekens <nerd@tdeekens.name>
2013-11-09 19:56:27 +0100

    Adds initial implementation of ChromeScribe.

--- lib/Scandio/lmvc/utils/logger/scribes/ChromeScribe.php
+++ lib/Scandio/lmvc/utils/logger/scribes/ChromeScribe.php
@@ -13,10 +13,12 @@ use Scandio\lmvc\utils\logger\loggers\LogLevel;
  */
 class ChromeScribe extends AbstractScribe
 {
-    public function scribe($message, $context, $level)
-    {
-        return false;
-    }
+    private
+        $_version           = null,
+        $_headersAccepted   = false,
+        $_initialized       = false,
+        $_headername        = null,
+        $_response          = null;
 
     /**
      * Initializes scribe and should always first initialize the parent (AbstractScribe).
@@ -29,5 +31,56 @@ class ChromeScribe extends AbstractScribe
         parent::initialize($config);
 
         $this->setLevel($config->level);
+        $this->_headername = $config->headername;
+        $this->_version = $config->version;
+    }
+
+    public function scribe($message, $context, $level)
+    {
+        if ( !$this->_omitMessage($level) ) {
+            $this->_response['rows'][] = $this->getFormatter()->format($message, $context);
+
+            $this->_process();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function _process()
+    {
+        if ($this->_initialized !== true) {
+            $this->_headersAccepted = $this->_acceptedByHeaders();
+            $this->_response['request_uri'] = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+
+            $this->_initialized = true;
+        }
+
+        $json = @json_encode($this->_response);
+        $data = base64_encode(utf8_encode($json));
+
+        $this->_sendWithHeaders($data);
+    }
+
+    private function _flushResponse()
+    {
+        $this->_response = [
+            'version'   => $this->_version,
+            'columns'   => ['label', 'log', 'backtrace', 'type'],
+            'rows'      => [],
+        ];
+    }
+
+    private function _acceptedByHeaders()
+    {
+        return !isset($_SERVER['HTTP_USER_AGENT']) || preg_match('{\bChrome/\d+[\.\d+]*\b}', $_SERVER['HTTP_USER_AGENT']);
+    }
+
+    private function _sendWithHeaders($data)
+    {
+        if ($this->_headersAccepted === true) {
+            header(sprintf('%s: %s', $this->_headername, $data));
+        }
     }
 }
